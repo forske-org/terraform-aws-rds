0e348f48b888d5fc1b736b5ff266ddf88a73a50a
Manuel Nielsen <MaNielsen@forske.org>
2024-09-27 10:02:47 +1000

    FEAT: created terraform variables file

---
+++ src/variables.tf
@@ -0,0 +1,9 @@
+variable "application" {
+    default = "<add application name>"
+    description = "The name of the application for resource naming and tagging"
+}
+
+variable "region" {
+    default = "<add aws region name>"
+    description = "AWS region"
+}
